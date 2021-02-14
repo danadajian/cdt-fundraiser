3cfb76d3adb1cd8314aa3d4eab5914594ad174d4
dadajian <v-dadajian@expediagroup.com>
2021-02-14 18:24:52 -0500

    fix script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash -e
 
-APP_BUCKET_NAME=tzedakah-boxes-app
 BOXES_BUCKET_NAME=tzedakah-boxes
+APP_BUCKET_NAME=tzedakah-boxes-app
+
+if aws s3api head-bucket --bucket "${BOXES_BUCKET_NAME}" 2>/dev/null
+then
+    echo "Bucket exists: $BOXES_BUCKET_NAME"
+else
+    echo "Bucket does not exist, creating: ${BOXES_BUCKET_NAME}"
+    aws s3 mb s3://"${BOXES_BUCKET_NAME}"
+    aws s3api put-bucket-cors --bucket "${BOXES_BUCKET_NAME}" --cors-configuration file://bucket-cors.json
+fi
 
 if aws s3api head-bucket --bucket "${APP_BUCKET_NAME}" 2>/dev/null
 then
@@ -12,15 +21,6 @@ else
     aws s3api put-bucket-policy --bucket "${APP_BUCKET_NAME}" --policy file://bucket-policy.json
     aws s3api put-bucket-cors --bucket "${APP_BUCKET_NAME}" --cors-configuration file://bucket-cors.json
     aws s3 website "s3://${APP_BUCKET_NAME}" --index-document index.html
-fi
-
-if aws s3api head-bucket --bucket "${BOXES_BUCKET_NAME}" 2>/dev/null
-then
-    echo "Bucket exists: $BOXES_BUCKET_NAME"
-else
-    echo "Bucket does not exist, creating: ${BOXES_BUCKET_NAME}"
-    aws s3 mb s3://"${BOXES_BUCKET_NAME}"
-    aws s3api put-bucket-cors --bucket "${BOXES_BUCKET_NAME}" --cors-configuration file://bucket-cors.json
 fi
 
 {
