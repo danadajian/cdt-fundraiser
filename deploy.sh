1a70e9848acf15466e4bbcbd8ba8397b02c5bcef
dadajian <v-dadajian@expediagroup.com>
2021-03-22 08:51:53 -0400

    add cdn invalidation

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,5 @@ else
     aws s3 website "s3://${BUCKET_NAME}" --index-document index.html
 fi
 
+aws cloudfront create-invalidation --distribution-id "${CDN_DISTRIBUTION_ID}" --paths /\*
 aws s3 sync ./build "s3://${BUCKET_NAME}" --exclude "precache-manifest*"
