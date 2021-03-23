aa0f40c4b28fdab3e34c9d0bd056c098fb0395c9
dadajian <v-dadajian@expediagroup.com>
2021-03-23 16:04:47 -0400

    improve submit button

--- src/components/PaymentSection.tsx
+++ src/components/PaymentSection.tsx
@@ -1,5 +1,5 @@
 import {StateProps} from "../types";
-import {Badge, Button, ButtonGroup, makeStyles, TextField} from "@material-ui/core";
+import {Badge, Button, ButtonGroup, makeStyles, TextField, Theme, Tooltip, withStyles} from "@material-ui/core";
 import {sum} from 'lodash';
 import {handlePayNowButton} from "../handlers/handlePayNowButton";
 import '../App.css';
@@ -21,11 +21,13 @@ export const PaymentSection = (props: StateProps) => {
             value={name}
             onChange={event => setName(event.target.value)}
         />
-        <ButtonGroup>
-            <Badge classes={{ badge: classes.badge }} badgeContent={`$${sum(state.selectedBoxNumbers)}`} showZero={false} max={100000}>
-                <Button size={'large'} color={'primary'} variant={'contained'} disableRipple={false}
-                        onClick={() => handlePayNowButton(props, name)}>Pay Now</Button>
-            </Badge>
+        <ButtonGroup style={{marginLeft: '0.5rem'}}>
+            <LightTooltip title={'Click "submit" when finished!'} arrow open>
+                <Badge classes={{ badge: classes.badge }} badgeContent={`$${sum(state.selectedBoxNumbers)}`} showZero={false} max={100000}>
+                    <Button style={{backgroundColor: '#040004'}} size={'large'} color={'primary'} variant={'contained'} disableRipple={false}
+                            onClick={() => handlePayNowButton(props, name)}>Submit</Button>
+                </Badge>
+            </LightTooltip>
             <Button size={'large'} color={'default'} variant={'outlined'}
                     onClick={() => setState({...state, selectedBoxNumbers: []})}>Clear</Button>
         </ButtonGroup>
@@ -35,6 +37,16 @@ export const PaymentSection = (props: StateProps) => {
 const useStyles = makeStyles({
     badge: {
         backgroundColor: '#F90505',
-        color: 'white'
+        color: 'white',
+        fontSize: 15
     }
 });
+
+const LightTooltip = withStyles((theme: Theme) => ({
+    tooltip: {
+        backgroundColor: '#F90505',
+        color: 'white',
+        boxShadow: theme.shadows[1],
+        fontSize: 20,
+    },
+}))(Tooltip);
