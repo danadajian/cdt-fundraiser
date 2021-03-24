27534cd07b7ba2a42fdbe80a420b9e800b8a125a
dadajian <v-dadajian@expediagroup.com>
2021-03-24 08:21:03 -0400

    add more instructions

--- src/components/PaymentConfirmation.tsx
+++ src/components/PaymentConfirmation.tsx
@@ -8,7 +8,7 @@ export const PaymentConfirmation = (props: {state: State}) => {
     const {name, selectedBoxNumbers} = props.state;
     return <div className={'PaymentConfirmation'}>
         <p>{`Agent ${name}: You've helped us complete the mission!`}</p>
-        <p style={{fontSize: 32}}>Remember your total amount and enter it on the next page.</p>
+        <p style={{fontSize: 32}}>Your boxes have been reserved. Remember your total amount and enter it on the next page.</p>
         <p style={{color: '#F90505'}}>{`Total Amount: $${sum(selectedBoxNumbers)}`}</p>
         <Button style={{backgroundColor: '#F90505', color: 'white'}}>
             <Link color={'inherit'} href={'https://www.dortamid.org/missionpossible2'} target={'_blank'}>
