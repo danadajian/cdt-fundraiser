f71ab3b845fa25100ded3c322138606862e742ea
dadajian <v-dadajian@expediagroup.com>
2021-03-26 07:51:25 -0500

    prevent race condition

---
+++ src/handlers/handlePayNowButton.test.ts
@@ -0,0 +1,82 @@
+import {handlePayNowButton} from "./handlePayNowButton";
+import {retrieveObjectFromS3, uploadObjectToS3} from "../aws/aws";
+import {BOXES_BUCKET_NAME, BOXES_TAKEN_FILE_NAME} from "../constants";
+
+jest.mock("../aws/aws");
+window.alert = jest.fn();
+
+const state = {
+    selectedBoxNumbers: [69],
+    boxesTaken: [
+        {name: 'Person 1', number: 1},
+        {name: 'Person 2', number: 90},
+        {name: 'Person 1', number: 300},
+        {name: 'Person 3', number: 88},
+        {name: 'Person 2', number: 6}
+    ],
+    paymentComplete: false
+};
+const setState = jest.fn();
+const name = 'My Name';
+
+describe('handlePayNowButton', () => {
+    describe('current state of the world matches', () => {
+        beforeEach(() => {
+            (uploadObjectToS3 as jest.Mock).mockImplementation(() => Promise.resolve());
+            (retrieveObjectFromS3 as jest.Mock).mockResolvedValue({
+                boxes: [
+                    {name: 'Person 1', number: 1},
+                    {name: 'Person 2', number: 90},
+                    {name: 'Person 1', number: 300},
+                    {name: 'Person 3', number: 88},
+                    {name: 'Person 2', number: 6}
+                ]
+            });
+            handlePayNowButton({state, setState}, name);
+        });
+
+        it('should call retrieveObjectFromS3 with correct params', () => {
+            expect(retrieveObjectFromS3).toHaveBeenCalledWith(BOXES_BUCKET_NAME, BOXES_TAKEN_FILE_NAME);
+        });
+
+        it('should call uploadObjectToS3 with correct params', () => {
+            expect(uploadObjectToS3).toHaveBeenCalledWith({boxes: [
+                    {name: 'Person 1', number: 1},
+                    {name: 'Person 2', number: 90},
+                    {name: 'Person 1', number: 300},
+                    {name: 'Person 3', number: 88},
+                    {name: 'Person 2', number: 6},
+                    {name, number: 69}
+                ]}, BOXES_BUCKET_NAME, BOXES_TAKEN_FILE_NAME);
+        });
+    });
+
+    describe('current state of the world does not match', () => {
+        beforeEach(() => {
+            (uploadObjectToS3 as jest.Mock).mockImplementation(() => Promise.resolve());
+            (retrieveObjectFromS3 as jest.Mock).mockResolvedValue({
+                boxes: [
+                    {name: 'Person 1', number: 1},
+                    {name: 'Person 2', number: 90},
+                    {name: 'Person 1', number: 300},
+                    {name: 'Person 3', number: 88},
+                    {name: 'Person 4', number: 73},
+                    {name: 'Person 2', number: 6}
+                ]
+            });
+            handlePayNowButton({state, setState}, name);
+        });
+
+        it('should not call uploadObjectToS3', () => {
+            expect(uploadObjectToS3).not.toHaveBeenCalled();
+        });
+
+        it('should alert the user', () => {
+            expect(window.alert).toHaveBeenCalled();
+        });
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+});
--- src/handlers/handlePayNowButton.ts
+++ src/handlers/handlePayNowButton.ts
@@ -1,5 +1,6 @@
-import {uploadObjectToS3} from "../aws/aws";
-import {StateProps} from "../types";
+import {retrieveObjectFromS3, uploadObjectToS3} from "../aws/aws";
+import {isEqual} from 'lodash';
+import {BoxesData, StateProps} from "../types";
 import {BOXES_TAKEN_FILE_NAME, BOXES_BUCKET_NAME} from "../constants";
 
 export const handlePayNowButton = (props: StateProps, name: string) => {
@@ -14,6 +15,12 @@ export const handlePayNowButton = (props: StateProps, name: string) => {
         return;
     }
     const newBoxesTaken = [...boxesTaken, ...selectedBoxNumbers.map(number => ({name, number}))];
-    return uploadObjectToS3({boxes: newBoxesTaken}, BOXES_BUCKET_NAME, BOXES_TAKEN_FILE_NAME)
-        .then(() => setState({...state, name, paymentComplete: true}));
+    return retrieveObjectFromS3(BOXES_BUCKET_NAME, BOXES_TAKEN_FILE_NAME)
+        .then((result: BoxesData) => {
+            if (isEqual(result.boxes, boxesTaken)) {
+                return uploadObjectToS3({boxes: newBoxesTaken}, BOXES_BUCKET_NAME, BOXES_TAKEN_FILE_NAME)
+                    .then(() => setState({...state, name, paymentComplete: true}));
+            }
+            alert('Some squares have recently been purchased. Please reload the page and try again.');
+        });
 };
