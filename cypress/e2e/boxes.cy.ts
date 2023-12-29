38bcd1e4d3ce9a5bcd52ba776b73cd3997a911db
danadajian <danadajian@gmail.com>
2023-12-28 22:42:12 -0600

    refactor test

--- cypress/e2e/boxes.cy.ts
+++ cypress/e2e/boxes.cy.ts
@@ -3,13 +3,10 @@ describe("boxes", () => {
     cy.visit("/");
   });
 
-  it("has the correct table format", () => {
+  it("displays an 18x20 table with numbers in reverse order", () => {
     cy.findByRole("table").should("be.visible");
     cy.findAllByRole("row").should("be.visible").and("have.length", 18);
     cy.findAllByRole("cell").should("be.visible").and("have.length", 360);
-  });
-
-  it("displays the cells in reverse order", () => {
     cy.findAllByRole("cell").first().should("have.text", "360");
     cy.findAllByRole("cell").last().should("have.text", "1");
   });
