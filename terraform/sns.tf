a6e252f154f73598cf7eed417f6b137fa90ba352
danadajian <danadajian@gmail.com>
2024-02-12 10:55:24 -0600

    add sns topic

---
+++ terraform/sns.tf
@@ -0,0 +1,19 @@
+resource "aws_sns_topic" "cdt_topic" {
+  name = "cdt-fundraiser-topic"
+}
+
+resource "aws_sns_topic_subscription" "cdt_subscription" {
+  topic_arn = aws_sns_topic.cdt_topic.arn
+  protocol  = "email"
+  endpoint  = "danadajian@gmail.com"
+}
+#resource "aws_sns_topic_subscription" "cdt_subscription" {
+#  topic_arn = aws_sns_topic.cdt_topic.arn
+#  protocol  = "email"
+#  endpoint = "rachelallencdt@gmail.com"
+#}
+#resource "aws_sns_topic_subscription" "cdt_subscription" {
+#  topic_arn = aws_sns_topic.cdt_topic.arn
+#  protocol  = "email"
+#  endpoint = "sjahanfar@dortamid.com"
+#}
