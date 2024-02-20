ab9d44fdac4a85d2d12d7ce51898fced77548777
danadajian <danadajian@gmail.com>
2024-02-20 07:38:18 -0600

    convert sns resources to for loop

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -3,17 +3,16 @@ resource "aws_sns_topic" "cdt_topic" {
 }
 
 resource "aws_sns_topic_subscription" "cdt_subscription" {
+  for_each  = toset(local.emails)
   topic_arn = aws_sns_topic.cdt_topic.arn
   protocol  = "email"
-  endpoint  = "danadajian@gmail.com"
+  endpoint  = each.value
 }
-resource "aws_sns_topic_subscription" "cdt_subscription_1" {
-  topic_arn = aws_sns_topic.cdt_topic.arn
-  protocol  = "email"
-  endpoint  = "rachelallencdt@gmail.com"
-}
-resource "aws_sns_topic_subscription" "cdt_subscription_2" {
-  topic_arn = aws_sns_topic.cdt_topic.arn
-  protocol  = "email"
-  endpoint  = "sjahanfar@dortamid.com"
+
+locals {
+  emails = [
+    "danadajian@gmail.com",
+    "rachelallencdt@gmail.com",
+    "sjahanfar@dortamid.com"
+  ]
 }
